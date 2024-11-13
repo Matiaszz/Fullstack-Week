e64de3ca92c589ee037e6f011d08326014ecf686
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-13 01:37:12 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 // Estrutura basica da pagina
 const Home = () => {
-  return <h1>Hello world!</h1>;
-}
+  // acessar estudar melhor a documentação do tailwind
+  return (
+    <div className="flex w-screen items-center justify-center bg-gray-800">
+      <h1 className="p-5 text-red-500">Hello world!</h1>
+    </div>
+  );
+};
 
 export default Home;
